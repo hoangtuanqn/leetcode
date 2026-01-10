ad874b7b8b5df4ce146b9abc037feb162e40e298
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-10 15:22:07 +0700

    Add project files.

---
+++ 300-code-exercises/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _300_code_exercises;
+
+var ok = new lesson1_two_sum();
+var data = ok.TwoSum([1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1], 11);
+foreach(var item in data)
+{
+    Console.WriteLine(item);
+}
---
+++ 300-code-exercises/lesson1_two_sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300_code_exercises
+{   
+    /*
+        Solution:
+            Sử dụng dictionary
+     */
+    internal class lesson1_two_sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var subtraction = target - nums[i];
+                if (map.TryGetValue(subtraction, out var result))
+                {
+                    return [result, i];
+                }
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+            return [];
+        }
+    }
+}
